2cf95d38d3b1dd0571a1d2484849231c44a90bfc
Jens <jense.92+github@gmail.com>
2018-08-29 15:28:58 +0200

    Show/Hide für Lernen des Probanden

--- js/hide.js
+++ js/hide.js
@@ -1,5 +1,20 @@
+var tempSequenceName;
+
 var hide = () =>{
-    console.log("hid");
-    console.log(document.getElementById("gpsinput").value.split("\\")[document.getElementById("gpsinput").value.split("\\").length - 1].split(".txt")[0]);
-    
+    $(`#hidebar`)["0"].style.display="none";
+    $(`#sqSettings`)["0"].style.display = "none";
+    $(`#hidecenter`)["0"].style.display = "none";
+    $(`#showcenter`)["0"].style.display = "";
+    $(`#body`)["0"].style.backgroundColor = "#f1f1f1";
+    tempSequenceName = $(`#sqeuenceName`)["0"].innerHTML;
+    $(`#sqeuenceName`)["0"].innerHTML = "Sequenz";    
+}
+
+var show = () => {
+    $(`#hidebar`)["0"].style.display = "";
+    $(`#sqSettings`)["0"].style.display = "";
+    $(`#hidecenter`)["0"].style.display = "";
+    $(`#showcenter`)["0"].style.display = "none";
+    $(`#body`)["0"].style.backgroundColor = "white";
+    $(`#sqeuenceName`)["0"].innerHTML = tempSequenceName;
 }
